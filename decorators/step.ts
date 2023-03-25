1cafa59c59e691b8f98e51ad4f44e8f2f0d2f708
Aleksey Egorov <57917516+pekanboy@users.noreply.github.com>
2023-03-25 15:24:37 +0300

    D-3 (#9)
    * Добавлена вложеность в декоратор step
    
    * added deploy to cdn
    
    ---------
    
    Co-authored-by: Egorov Aleksey <aleksey.egorov@vk.team>

--- decorators/step.ts
+++ decorators/step.ts
@@ -2,13 +2,12 @@ import {LoggerService} from "../servises/LoggerService/LoggerService.js";
 
 const step = (stepLog: string) => {
     return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<Function>) => {
-        const targetFunction = descriptor.value?.bind(target);
+        const targetFunction = descriptor.value;
         
+        // TODO: Добавить поддержку дефолтных параметров
         descriptor.value = (...args: any[]) => {
             return new Promise(async (resolve, reject) => {
-                resolve(await LoggerService.step(stepLog, async () => {
-                    await targetFunction?.(...args)
-                }))
+                resolve(await LoggerService.step(stepLog, async () => await targetFunction?.apply(target, args)));
             });
         };
     }
